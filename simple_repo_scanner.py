a9574f8bc6ba0eb238e7d23da5efcfcdefae8664
Sohanuu66 <asohankumar34@gmail.com>
2025-05-18 20:12:20 +0530

    Update simple_repo_scanner.py

--- simple_repo_scanner.py
+++ simple_repo_scanner.py
@@ -2,8 +2,8 @@ import requests
 import pandas as pd
 import os
 
-GITHUB_TOKEN = os.getenv("MY_GITHUB_TOKEN")
-ORG_NAME = os.getenv("MY_GITHUB_ORG")
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+ORG_NAME = os.getenv("GITHUB_ORG")
 
 API_URL = f"https://api.github.com/orgs/{ORG_NAME}/repos"
 HEADERS = {
@@ -36,7 +36,6 @@ def get_repos():
 def save_to_excel(data):
     df = pd.DataFrame(data)
     df.to_excel("repos_report.xlsx", index=False)
-    print("Report saved as repos_report.xlsx")
 
 if __name__ == "__main__":
     repos = get_repos()
